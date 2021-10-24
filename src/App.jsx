ee09af2e5e12326cc7767739044cdd9107569d92
Manuela Rodriguez-Acosta Cassinello <mrac.in@icloud.com>
2021-10-24 19:15:13 +0200

    did intro page

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Testimonials from "./components/testimonials/Testimonials";
+import Community from "./components/community/Community";
+import "./app.scss";
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar />
+      <div className="sections">
+        <Intro />
+        <Testimonials />
+        <Community />
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
