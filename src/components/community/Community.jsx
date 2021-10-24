5ecb24b771507d3837b6ee7a345f502108748132
Manuela Rodriguez-Acosta Cassinello <mrac.in@icloud.com>
2021-10-24 20:35:47 +0200

    finished community

--- src/components/community/Community.jsx
+++ src/components/community/Community.jsx
@@ -3,8 +3,21 @@ import "./community.scss"
 export default function Community() {
   return (
     <div className="community" id="community">
+      <div className="t1">
+        <img src="assets/3.png" alt="k"></img>
+      </div>
+       <div className="profile">
+        <img src="assets/4.png" alt="k"></img>
+        <img src="assets/4.png" alt="k"></img>
+        <img src="assets/4.png" alt="k"></img>
+        <img src="assets/4.png" alt="k"></img>
+
+       </div>
+      <h1>Contact us | MyOther.inc</h1>
+
 
 
     </div>
+
   );
 }
